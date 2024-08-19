4a480f078ce3e18c2aa653ab1facb87ad85fb10f
Mariana Estevam <martins.mariih194@gmail.com>
2024-08-19 08:30:15 -0300

    Primeira versão do projeto

---
+++ src/test/java/br/com/usetdm/UsetdmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.usetdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsetdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
