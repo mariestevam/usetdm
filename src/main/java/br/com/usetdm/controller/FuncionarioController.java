c70116c82c4f2b462b81da284494868467795c2d
Mariana Estevam <martins.mariih194@gmail.com>
2024-08-27 10:07:18 -0300

    criação do excluir e inserir na listagem de funcionario

--- src/main/java/br/com/usetdm/controller/FuncionarioController.java
+++ src/main/java/br/com/usetdm/controller/FuncionarioController.java
@@ -1,11 +1,14 @@
 package br.com.usetdm.controller;
 
+import br.com.usetdm.model.Funcionario;
 import br.com.usetdm.repository.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/funcionario")
@@ -18,5 +21,27 @@ public class FuncionarioController {
     public String index(Model model){
         model.addAttribute("funcionarios", funcionarioRepository.findAll());
         return "funcionario/index";
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluir(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
+
+        // Busca o responsável no banco de dados pelo id
+        Funcionario responsavel = funcionarioRepository.findById(id).get();
+
+        // Remove o responsável do banco de dados
+        funcionarioRepository.delete(responsavel);
+
+        // Adiciona uma mensagem que será exibida no template
+        redirectAttributes.addFlashAttribute("mensagem", "Funcionario excluído com sucesso!");
+
+        // Redireciona para a página de listagem de responsáveis
+        return "redirect:/funcionario";
+    }
+
+    @GetMapping("/form-inserir")
+    public String cadastrar(Model model){
+        model.addAttribute("funcionario", new Funcionario());
+        return "funcionario/form-inserir";
     }
 }
