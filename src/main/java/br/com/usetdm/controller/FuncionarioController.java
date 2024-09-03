3ab4daa22e4f58b6cfeb3452a921c698bd99b53e
Mariana Estevam <martins.mariih194@gmail.com>
2024-09-03 16:17:50 -0300

    atualização

--- src/main/java/br/com/usetdm/controller/FuncionarioController.java
+++ src/main/java/br/com/usetdm/controller/FuncionarioController.java
@@ -4,6 +4,7 @@ import br.com.usetdm.model.Funcionario;
 import br.com.usetdm.repository.FuncionarioRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.Id;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @Controller
 @RequestMapping("/funcionario")
@@ -65,6 +68,7 @@ public class FuncionarioController {
     }
 
     // Formulário de Alteração dos Jogadores
+
     @GetMapping("/form-alterar/{id}")
     public String formAlterar(@PathVariable("id") Long id, Model model){
 
@@ -85,6 +89,19 @@ public class FuncionarioController {
         if(result.hasErrors()){
             return "funcionario/form-alterar";
         }
+        // Busca o aluno no banco de dados
+        Funcionario funcionarioAtualizado = funcionarioRepository.findById(funcionario.getId()).orElseThrow(() -> new IllegalArgumentException("ID inválido"));
+
+
+        // Seta os dados do aluno
+
+        funcionarioAtualizado.setNome(funcionario.getNome());
+        funcionarioAtualizado.setEmail(funcionario.getEmail());
+        funcionarioAtualizado.setCpf(funcionario.getCpf());
+        funcionarioAtualizado.setTelefone(funcionario.getTelefone());
+
+        // Salva o aluno no banco de dados
+        funcionarioRepository.save(funcionarioAtualizado);
 
         funcionarioRepository.save(funcionario);
         redirectAttributes.addFlashAttribute("mensagem", "Funcionario alterado com sucesso!");
